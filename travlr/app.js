1220964383ad30377d55a43cc9e83e1f92543082
Jessica Johnson <jessica.johnson75@snhu.edu>
2026-04-18 10:50:40 -0500

    Project 2 final submission - Angular SPA, API, JWT auth, SDD updates

--- travlr/app.js
+++ travlr/app.js
@@ -7,10 +7,12 @@ const path = require('path');
 const hbs = require('hbs');
 const ctrlTravel = require('./app_server/controllers/travel');
 const apiRouter = require('./app_api/routes/index');
+const cors = require('cors');
 
 /* Create an Express application */
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
